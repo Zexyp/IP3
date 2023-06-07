a64d8d4edde7d3fa36dea026191e7718ec209a34
Pjavel <zexyp01@gmail.com>
2023-06-07 16:30:49 +0200

    starting

---
+++ PrevenceReakceDetekceEvidenceLogistika/config/enums.php
@@ -0,0 +1,9 @@
+<?php
+
+enum Role: int {
+    case ADMIN = 0;
+    case EMPLOYEE = 1;
+    case RECEIVER = 2;
+    case COURIER = 3;
+    case RESPONDER = 4;
+}
--- PrevenceReakceDetekceEvidenceLogistika/database/migrations/2014_10_12_000000_create_users_table.php
+++ PrevenceReakceDetekceEvidenceLogistika/database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -17,9 +18,20 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        // Insert some stuff
+        DB::table('users')->insert(
+            array(
+                'name' => 'admin',
+                'email' => 'pog@poggers',
+                'password' => '$2y$10$7K9Etf67P/v6xFWqvUdbceDGHhfICs2HqVkC0Jdo8zZADnaBQ0Lyu',
+                'role' => 0
+            )
+        );
     }
 
     /**
